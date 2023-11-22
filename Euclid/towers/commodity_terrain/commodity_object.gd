2f294f353c95afa1399bab2957fa47ab79d24ecf
AthertonRoamer <111521556+AthertonRoamer@users.noreply.github.com>
2023-11-21 19:26:59 -0600

    modified commodity objects so they dont have collision

--- Euclid/towers/commodity_terrain/commodity_object.gd
+++ Euclid/towers/commodity_terrain/commodity_object.gd
@@ -1,34 +1,20 @@
 class_name CommodityObject
-extends Tower
+extends Node2D
 
 @export var commodity : Commodity
 
-#var first_frame := true
+var tile_pos : Vector2
+
 
 func _ready() -> void:
-	super()
-#	GameState.set_up_commodities.connect(_on_world_ready)
+	tile_pos = (global_position - GameState.real_tile_size / 2).snapped(GameState.real_tile_size) / GameState.real_tile_size
 	add_to_group("commodity_object")
 	set_up_commodity()
-	
-	
-#func _process(delta):
-#	if first_frame:
-#		first_frame = false
-#		set_up_commodity()
-	
-	
-	
-func _on_world_ready() -> void:
-	print("setting up commodity")
-#	set_up_commodity()
 
 	
 func set_up_commodity() -> void:
 	GameState.front_tile_map.set_tile_commodity_data(tile_pos, self)
-#	print(str(GameState.front_tile_map))
 	
 	
 func _exit_tree() -> void:
-	super()
 	GameState.front_tile_map.set_tile_commodity_data(tile_pos)
