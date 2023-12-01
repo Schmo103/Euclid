0a1190a0da4cb9e4cd2f14a4fdfbf4b7c2065171
Schmo103 <112767599+Schmo103@users.noreply.github.com>
2023-12-01 00:52:32 -0600

    Revert "Merge pull request #58 from Schmo103/owens"
    This reverts commit e5783c79305f422122635cb864b57d7503101424, reversing
    changes made to cfd96cb733653276d7dbb5a5e5cd0b6b9380bcb0.

--- Euclid/enemies/tank/tank.gd
+++ Euclid/enemies/tank/tank.gd
@@ -1,52 +1,10 @@
 class_name Tank
 extends StandardEnemy
 
-func _ready():
-	super()
-	anti_jit.timeout.connect(_on_anti_jit_timeout)
-	
-	
 
-func update_visuals(v : Vector2) -> void:
-	var angle = v.angle()
-	var direction = ""
 
-	if -PI/4 <= angle and angle < PI/4:
-		direction = "right"
-	elif PI/4 <= angle and angle < 3*PI/4:
-		direction = "down"
-	elif -3*PI/4 <= angle and angle < -PI/4:
-		direction = "up"
-	else:
-		direction = "left"
-
-	match direction:
-		"up":
-			attack_dir = "up"
-			left_right_v.visible = false
-			up_v.visible = true
-			down_v.visible = false
-			anim_player.play("up_walk")
-		"down":
-			attack_dir = "down"
-			left_right_v.visible = false
-			up_v.visible = false
-			down_v.visible = true
-			anim_player.play("down_walk")
-		"left":
-			attack_dir = "left"
-			left_right_v.visible = true
-			up_v.visible = false
-			down_v.visible = false
-			left_right_v.scale.x = normal_scale.x * -1
-			anim_player.play("walk_left_right")
-		"right":
-			attack_dir = "right"
-			left_right_v.visible = true
-			up_v.visible = false
-			down_v.visible = false
-			left_right_v.scale.x = normal_scale.x * 1
-			anim_player.play("walk_left_right")
+func update_visuals(_v : Vector2) -> void:
+	pass
 	
 	
 func attack_clean_up() -> void:
@@ -57,13 +15,6 @@ func attack_target(target : Node) -> void:
 	if is_instance_valid(target):
 		attacking = true
 		target.take_damage(damage_dealt)
-
-	
-	
-func _on_anti_jit_timeout() -> void:
-	if !GameState.game_over:
-		if !attacking:
-			update_visuals(anim_dir)
 	
 	
 	
