7b45ae01a5eafd0423be8465d5e0520f5a58f4bc
AthertonRoamer <111521556+AthertonRoamer@users.noreply.github.com>
2023-12-01 00:27:52 -0600

    Added tutorial among other things

--- Euclid/enemies/standard/attacking.gd
+++ Euclid/enemies/standard/attacking.gd
@@ -23,18 +23,25 @@ func initiate_state() -> void:
 	super()
 	enemy.attacking = true
 	attacking_timer.start()
-	enemy.attack_target(target)
+	if is_instance_valid(enemy):
+		if is_instance_valid(target):
+			if target is Node:
+				enemy.attack_target(target)
+		else:
+			print("passed in a non node target to enemy.attack_target()")
 	
 func clean_up():
 	super()
-	enemy.attack_clean_up()
+	if is_instance_valid(enemy):
+		enemy.attack_clean_up()
 #	enemy.get_node("AnimationPlayer").play("RESET")
 #	enemy.get_node("AnimationPlayer").play("UP_RESET")
 	
 #when attack has been carried out, returns to fighting state to plan next attack
 func _on_attacking_timer_timed_out() -> void:
-	enemy.state_machine.transition_to(fighting)
-	enemy.attacking = false
+	if is_instance_valid(enemy):
+		enemy.state_machine.transition_to(fighting)
+		enemy.attacking = false
 	
 	
 	
--- Euclid/menu/menu.gd
+++ Euclid/menu/menu.gd
@@ -8,4 +8,5 @@ func _ready() -> void:
 	
 
 func _on_start_pressed():
+	GameState.using_tutorial = $CheckBox.button_pressed
 	get_tree().change_scene_to_packed(world)
